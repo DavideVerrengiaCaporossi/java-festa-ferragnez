8eafd2d8ed20e276bfb6f4895a3f9de7817d06f9
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-10 15:18:51 +0200

    correzione nome  pacchetto

---
+++ src/com/ferragnez/party/FestaFerragnez.java
@@ -0,0 +1,28 @@
+package com.ferragnez.party;
+
+public class FestaFerragnez {
+
+	public static void main(String[] args) {
+		
+		
+		//creazione array con partecipanti
+		
+		String[] listaInvitati = new String [10];
+		
+		listaInvitati[0] = ("Dua Lipa");
+		listaInvitati[1] = ("Paris Hilton");
+		listaInvitati[2] = ("Manuel Agnelli");
+		listaInvitati[3] = ("J-Ax");
+		listaInvitati[4] = ("Francesco Totti");
+		listaInvitati[5] = ("Ilary Blasi");
+		listaInvitati[6] = ("Bebe Vio");
+		listaInvitati[7] = ("Luis");
+		listaInvitati[8] = ("Pardis Zarei");
+		listaInvitati[9] = ("Martina Maccherone");
+		listaInvitati[10] = ("Rachel Zeilic");
+		
+		
+
+	}
+
+}
